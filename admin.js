a579c9967c9448fbdb2100e6e9f674faf0fdf3b8
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2022-09-29 23:16:06 +0300

    Update multiple rules for 1 field

--- admin.js
+++ admin.js
@@ -1,9 +1,28 @@
-function Validator(option){
+function Validator(option) {
 
-    function validate(selectorElement,rule){
+    var listSelectors = {};
+
+    function validate(selectorElement, rule) {
         var errorElement = selectorElement.parentElement.querySelector(option.message);
-        var errorMessage = rule.check(selectorElement.value);
-        if (errorMessage){
+        var errorMessage;
+
+        var rules = listSelectors[rule.selector];
+
+
+
+        for (var i = 0; i < rules.length; ++i) {
+
+
+            errorMessage = rules[i](selectorElement.value);
+
+            if (errorMessage) {
+                break;
+            }
+
+
+        }
+
+        if (errorMessage) {
             errorElement.innerText = errorMessage;
         } else {
             errorElement.innerText = '';
@@ -11,47 +30,83 @@ function Validator(option){
     }
 
     var formElement = document.querySelector(option.form);
-    console.log(formElement);
+    //console.log(formElement);
 
-    if (formElement){
+    if (formElement) {
+
+        formElement.onsubmit = function(e){
+            e.preventDefault();
+
+            option.rules.forEach(rule => {
+                var selectorElement = formElement.querySelector(rule.selector);
+                validate(selectorElement, rule);
+        })
+    }
+
+
         option.rules.forEach(rule => {
             var selectorElement = formElement.querySelector(rule.selector);
-            console.log(selectorElement);
-            
-            selectorElement.onblur = function(){
-                validate(selectorElement,rule);
-               
-                selectorElement.oninput = function(){
-                    var errorElement = selectorElement.parentElement.querySelector(option.message);
-                    errorElement.innerText = '';
-                }
-                
-                //console.log(errorElement );
 
-                //console.log(rule.check(selectorElement.value)); 
+            if (Array.isArray(listSelectors[rule.selector])) {
+                listSelectors[rule.selector].push(rule.check);
+            } else {
+                listSelectors[rule.selector] = [rule.check];
             }
 
 
-            
+            //console.log(selectorElement);
+
+            selectorElement.onblur = function () {
+                validate(selectorElement, rule);
+
+                selectorElement.oninput = function () {
+                    var errorElement = selectorElement.parentElement.querySelector(option.message);
+                    errorElement.innerText = '';
+                }
+
+                console.log(listSelectors);
+
+            }
+
+
+
         });
     }
 }
 
-Validator.isRequired = function(selector){
+Validator.isRequired = function (selector, message) {
     return {
         selector: selector,
-        check: function(value){
-            return value.trim() ? undefined : 'Vui lòng nhập tên';
+        check: function (value) {
+            return value.trim() ? undefined : message || 'Vui lòng nhập trường này';
         }
     }
 };
 
-Validator.isEmail = function(selector){
+Validator.isEmail = function (selector) {
     return {
         selector: selector,
-        check: function(value){
+        check: function (value) {
             let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
             return (value.match(regex)) ? undefined : 'Vui lòng nhập email'
         }
     }
 };
+
+Validator.isPassword = function (selector, minLength) {
+    return {
+        selector: selector,
+        check: function (value) {
+            return value.length >= minLength ? undefined : 'Vui lòng nhập trên ' + minLength + ' kí tự';
+        }
+    }
+}
+
+Validator.isConfirm = function (selector, selectorPassword, message) {
+    return {
+        selector: selector,
+        check: function (value) {
+            return value === selectorPassword() ? undefined : message || 'Giá trị nhập không chính xác ';
+        }
+    }
+}
